a159d4391d1eac97e3701db7f8b9fe7cf37ef57f
OmriStein <omri.stein@gmail.com>
2021-04-06 22:33:05 +0300

    added documentation

--- FatigueAnalysis/Stress.py
+++ FatigueAnalysis/Stress.py
@@ -1,32 +1,29 @@
 # TODO: better stress calculation
 def UniformStress(F, A):
-    """ Returns stress assuming uniform distribution in cross section
+    """Returns stress assuming uniform distribution in cross section
 
-        :keyword F: Force
-        :keyword A: Cross section area
-        :returns Stress
-        :rtype: float
-        """
+    :param float F: Force
+    :param foat A: Cross section area
+
+    :returns Stress
+    :rtype: float
+    """
     return F / A
 
 
 def BendingStress(My, Iy, z, Mz=None, Iz=None, y=None):
-    """ Bending stress in a principle system
+    """Bending stress in a principle system
 
-        :keyword My: Moment in the y direction
-        :type My: float
-        :keyword Mz: Moment in the y direction
-        :type Mz: float
-        :keyword Iy: Area moment of inertia for the y direction
-        :type Iy: float
-        :keyword Iz: Area moment of inertia for the zeta direction
-        :type Iz: float
-        :keyword z: Coordinate
-        :type z: float
-        :keyword y: Coordinate
-        :type y: float
-        :returns: Bending Stress in a cross section
-        :rtype: float """
+    :param float My: Moment in the y direction
+    :param float Mz: Moment in the y direction
+    :param float Iy: Area moment of inertia for the y direction
+    :param float Iz: Area moment of inertia for the zeta direction
+    :param float z: Coordinate
+    :param float y: Coordinate
+
+    :returns: Bending Stress in a cross section
+    :rtype: float
+    """
 
     if Mz is None:
         return (My / Iy) * z
@@ -37,30 +34,41 @@ def BendingStress(My, Iy, z, Mz=None, Iz=None, y=None):
 def ShearBendingStress(V, Q, I, b):
     """ Shear stresses due to bending
 
-        :keyword V: Shear force in y direction
-        :type V: float
-        :keyword Q: first moment of in cross section in y direction
-        :type Q: float
-        :keyword I: Area moment of inertia around the y axis
-        :type I: float
-        :keyword b: thickness
-        :type b: float
-        :returns: Shear stress resulting from bending
-        :rtype: float"""
+    :param float V: Shear force in y direction
+    :param float Q: first moment of in cross section in y direction
+    :param float I: Area moment of inertia around the y axis
+    :param float b: thickness
+
+    :returns: Shear stress resulting from bending
+    :rtype: float
+    """
 
     return (V * Q) / (I * b)
 
 
 def TorsionStress(T, r, J):
     """ Torsion stresses
-        :keyword T: Cross section torque
-        :keyword r: Stress radius coordinate
-        :keyword J: Cross section polar moment of inertia
-        :returns: Torsion Stress at r """
+
+    :param float T: Cross section torque
+    :param float r: Stress radius coordinate
+    :param float J: Cross section polar moment of inertia
+
+    :returns: Torsion Stress at r
+    :rtype: float
+    """
     return (T * r) / J
 
 
 def MaxShearStress(V, A, shape):
+    """Returns The maximum shear stress for known shapes
+
+    :param float V: Shear Stress
+    :param float A: Cross section area
+    :param str shape: Cross section shape
+
+    :returns: Maximum shear Stress
+    :rtype: float
+    """
     if shape == 'circle':
         return (4 * V) / (3 * A)
     elif shape == ' rectangle':
