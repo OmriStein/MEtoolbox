ad37e720393ecee64fbc0739221b9c5aa719b6c4
OmriStein <omri.stein@gmail.com>
2021-04-23 13:32:28 +0300

    added Sp and Sut attributes based on the bolt's grade

--- me_toolbox/fasteners/metric_bolt.py
+++ me_toolbox/fasteners/metric_bolt.py
@@ -1,4 +1,5 @@
 """module containing the MetricBolt class"""
+from collections import namedtuple
 from math import pi
 
 from me_toolbox.fasteners import Bolt
@@ -6,20 +7,32 @@ from me_toolbox.fasteners import Bolt
 
 class MetricBolt(Bolt):
     """MetricBolt contains the attributes for a metric bolt"""
+    grade = namedtuple('Grade', ['low', 'high', 'Sp', 'Sut', 'Sy'])
+    grade_list = {'4.6': grade(5, 36, 225, 400, 240),
+                  '4.8': grade(1.6, 16, 310, 420, 340),
+                  '5.8': grade(5, 24, 380, 520, 420),
+                  '8.8': grade(16, 36, 600, 830, 660),
+                  '9.8': grade(1.6, 16, 650, 900, 720),
+                  '10.9': grade(5, 36, 830, 1040, 940),
+                  '12.9': grade(1.6, 36, 970, 1220, 1100)}
+
     def __repr__(self):
         return f"MetricBolt(M{self.diameter}x{self.pitch} {self.fit} {self.tolerance})"
 
-    def __init__(self, diameter, pitch, fit, tolerance, length, elastic_modulus=210e3):
+    def __init__(self, diameter, pitch, fit, tolerance, length, grade, elastic_modulus=210e3):
         """Initializing a UnBolt object
 
         :param float diameter: Major nominal diameter
         :param float pitch: Thread's pitch
-        :param str fit: the fit between the nut and bolt
-        :param int tolerance: The fit tolerance
+        :param str fit: The fit between the nut and bolt
+        :param int tolerance: Bolt's fit tolerance
+        :param float length: Bolt's length
+        :param str grade: Bolt's grade
         """
         super().__init__(diameter, pitch, length, elastic_modulus)
         self.fit = fit
         self.tolerance = tolerance
+        self.grade = grade
 
     @property
     def thread_length(self):
@@ -45,3 +58,13 @@ class MetricBolt(Bolt):
         """Minor diameter area (Ar)"""
         dr = self.diameter - 1.226869 * self.pitch
         return 0.25 * pi * dr ** 2
+
+    @property
+    def proof_strength(self):
+        """Minimum proof strength"""
+        return self.grade_list[self.grade].Sp
+
+    @property
+    def tensile_strength(self):
+        """Minimum tensile strength"""
+        return self.grade_list[self.grade].Sut
--- me_toolbox/fasteners/un_bolt.py
+++ me_toolbox/fasteners/un_bolt.py
@@ -1,4 +1,5 @@
 """module containing the UnBolt class"""
+from collections import namedtuple
 from math import pi
 
 from me_toolbox.fasteners import Bolt
@@ -6,26 +7,37 @@ from me_toolbox.fasteners import Bolt
 
 class UnBolt(Bolt):
     """UnBolt contains the attributes for a Unified National(UN) bolt"""
+    grade = namedtuple('Grade', ['low', 'high', 'Sp', 'Sut', 'Sy'])
+    grade_list = {'1': grade(0.25, 1.5, 33, 60, 36),
+                  '2': (grade(0.25, 0.75, 55, 74, 57), grade(7 / 8, 1.5, 33, 60, 36)),
+                  '4': grade(0.25, 1.5, 65, 115, 100),
+                  '5': (grade(0.25, 1, 85, 120, 92), grade(9 / 8, 1.5, 74, 105, 81)),
+                  '5.2': grade(0.25, 1, 85, 120, 92),
+                  '7': grade(0.25, 1.5, 105, 133, 115),
+                  '8': grade(0.25, 1, 120, 150, 130)}
 
     def __repr__(self):
         return f"UnBolt({self.diameter}\"-{self.tpi} " \
                f"{'UNF' if self.fine else 'UNC'} {self.fit}" \
                f"{'B' if self.bolt else 'A'})"
 
-    def __init__(self, diameter, tpi, fine, fit, bolt, length, elastic_modulus=30):
+    def __init__(self, diameter, tpi, fine, fit, bolt, length, grade, elastic_modulus=30):
         """Initializing a UnBolt object
 
         :param float diameter: major nominal diameter
         :param float tpi: Tooth per inch
         :param bool fine: True if fine or False if course
-        :param int fit: the fit between the nut and bolt (1/2/3)
+        :param int fit: Fit between the nut and bolt (1/2/3)
         :param bool bolt: True if bolt False if nut
+        :param float length: Bolt's length
+        :param str grade: Bolt's grade
         """
         super().__init__(diameter, 1 / tpi, length, elastic_modulus)
         self.tpi = tpi
         self.fine = fine
         self.fit = fit
         self.bolt = bolt
+        self.grade = grade
 
     @property
     def thread_length(self):
@@ -46,3 +58,27 @@ class UnBolt(Bolt):
         """Minor diameter area (Ar)"""
         dr = self.diameter - 1.299038 * self.pitch
         return 0.25 * pi * dr ** 2
+
+    @property
+    def proof_strength(self):
+        """Minimum proof strength"""
+        d = self.diameter
+        if len(self.grade_list[self.grade]) == 2:
+            for i in range(2):
+                if self.grade_list[self.grade][i].low < d < self.grade_list[self.grade][i].high:
+                    return self.grade_list[self.grade][i].Sp
+            raise ValueError("The bolt diameter don't match bolt grade")
+        else:
+            return self.grade_list[self.grade].Sp
+
+    @property
+    def tensile_strength(self):
+        """Minimum tensile strength"""
+        d = self.diameter
+        if len(self.grade_list[self.grade]) == 2:
+            for i in range(2):
+                if self.grade_list[self.grade][i].low < d < self.grade_list[self.grade][i].high:
+                    return self.grade_list[self.grade][i].Sp
+            raise ValueError("The bolt diameter don't match bolt grade")
+        else:
+            return self.grade_list[self.grade].Sut
