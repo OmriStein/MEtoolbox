c8228b10783d9ba0bfd66776ec86702f209db30d
OmriStein <omri.stein@gmail.com>
2024-03-10 16:06:30 +0200

    Minor changes

--- examples/fasteners_examples/BoltPattern_example.py
+++ examples/fasteners_examples/BoltPattern_example.py
@@ -18,20 +18,24 @@ fasteners_locations = [[20, 45, 0], [-20, 45, 0], [0, 15, 0]]
 force = [0, -8500, 0]  # [N]
 preloads = [32062.5, 32062.5, 7850]  # [N]
 force_location = [0, 0, 100]
-tilting_edge = [[0, 0], [1, 0]]
-pattern = BoltPattern(fasteners, fasteners_locations, force, preloads, force_location, tilting_edge,
+axis_of_rotation = [[0, 0], [1, 0]]  # Two points on the axis of rotation
+pattern = BoltPattern(fasteners, fasteners_locations, force, preloads, force_location, axis_of_rotation,
                       'shank')
 
 print(f"M5: ld={M5.shank_length}, lt={M5_fastener.griped_thread_length},"
       f"Ad={M5.nominal_area:.2f}, At={M5.stress_area:.2f}, kb={M5_fastener.bolt_stiffness:.2f},"
       f"km={M5_fastener.member_stiffness:.2f}, C={M5_fastener.fastener_stiffness:.3f}")
+
 print(f"M10: ld={M10.shank_length}, lt={M10_fastener.griped_thread_length},"
       f"Ad={M10.nominal_area:.2f}, At={M10.stress_area:.2f}, kb={M10_fastener.bolt_stiffness:.2f},"
       f"km={M10_fastener.member_stiffness:.2f}, C={M10_fastener.fastener_stiffness:.3f}")
+
 print(f"Fi={pattern.total_shear_force}\n\u03C4={pattern.shear_stress}")
 print(f"P={pattern.fastener_load}\nFb={pattern.bolt_load}")
+
 print(f"\u03C3={pattern.normal_stress}")
 print(f"\u03C3eq={pattern.equivalent_stresses}")
+
 M10_nL = M10_fastener.load_safety_factor(preloads[0], pattern.equivalent_stresses[0])
 M5_nL = M5_fastener.load_safety_factor(preloads[2], pattern.equivalent_stresses[2])
 print(f"M10_nL={M10_nL:.2f},M5_nL={M5_nL:.2f}")
