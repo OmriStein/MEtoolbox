838cbff93358c7f1f6736fb7ba2fcec061a9c6fc
OmriStein <omri.stein@gmail.com>
2024-03-21 00:08:31 +0200

    removed calc_kf as it is now a part of the FatigueAnalysis class

--- me_toolbox/fatigue/__init__.py
+++ me_toolbox/fatigue/__init__.py
@@ -1,5 +1,5 @@
 from me_toolbox.fatigue.failure_criteria import FailureCriteria
-from me_toolbox.fatigue.fatigue_analysis import FatigueAnalysis, calc_kf
+from me_toolbox.fatigue.fatigue_analysis import FatigueAnalysis
 from me_toolbox.fatigue.endurance_limit import EnduranceLimit
 from me_toolbox.fatigue.stress import *
 
