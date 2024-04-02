46b0dbc42162e2b7754350474a2b78bb2c6997d0
OmriStein <omri.stein@gmail.com>
2024-04-02 21:25:14 +0300

    replaced Ap and m with ultimate tensile strength in Spring class call

--- examples/springs_examples/HelicalPushSpring_example3.py
+++ examples/springs_examples/HelicalPushSpring_example3.py
@@ -1,4 +1,4 @@
-from me_toolbox.springs import HelicalPushSpring
+from me_toolbox.springs import HelicalPushSpring, Spring
 from sympy import symbols, Eq, solveset
 
 outer_diameter = 14.29
@@ -7,14 +7,16 @@ D = outer_diameter - d
 L0 = 111.12
 
 # Chrome-vanadium wire
-G, E, Ap, m, yield_percent = 77.2e3, 203.4, 2005, 0.168, 0.45
+G, E, yield_percent = 77.2e3, 203.4, 0.45
+Sut = Spring.material_prop('chrome-vanadium wire', 6.5, metric=True, verbose=True)
 
 K = symbols('K')
 Fmax = 105.7  # [N]
 Fmin = 12.24  # [N]
 spring = HelicalPushSpring(max_force=Fmax, wire_diameter=d, spring_diameter=D,
+                           ultimate_tensile_strength=Sut,
                            shear_yield_percent=yield_percent, end_type='squared and ground',
-                           shear_modulus=G, elastic_modulus=E, Ap=Ap, m=m, spring_constant=None,
+                           shear_modulus=G, elastic_modulus=E, spring_constant=None,
                            active_coils=21, free_length=L0, set_removed=False, shot_peened=False)
 
 nf, ns = spring.fatigue_analysis(Fmax, Fmin, 99, verbose=True)
