1b977911052d0997aef983287e989936e61a0e7f
OmriStein <omri.stein@gmail.com>
2024-05-05 20:33:23 +0300

    update version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="me_toolbox",
-    version="0.0.15",
+    version="0.0.16",
     author="Omri Stein",
     author_email="omri.stein@gmail.com",
     description="Mechanical engineering design tools",
